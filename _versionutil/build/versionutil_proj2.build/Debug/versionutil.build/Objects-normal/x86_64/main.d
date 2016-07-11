14ef73fc00a0ad5408f6bd4c6f68ae3b4552310f
Aaron Smith <gngrwzrd@gmail.com>
2016-07-10 22:07:48 -0700

    Removed old xcode proj

--- _versionutil/build/versionutil_proj2.build/Debug/versionutil.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/aaronsmith/Development/Aaron/versionutil/_versionutil/versionutil_proj2/main.c
--- _versionutil/build/versionutil_proj2.build/Debug/versionutil_proj2.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/aaronsmith/Development/Aaron/versionutil/versionutil_proj2/versionutil_proj2/main.c
